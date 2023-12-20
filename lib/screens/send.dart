fd0777fc49d9985ea5ec8c6890925cbab52f00d4
JoshuaH-sudo <JHwork101@protonmail.com>
2023-12-20 21:32:15 +1100

    Add main page ui elements.

--- lib/screens/send.dart
+++ lib/screens/send.dart
@@ -1,8 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class SendScreen extends StatelessWidget {
+class SendScreen extends StatefulWidget {
   const SendScreen({super.key});
+
+  @override
+  State<SendScreen> createState() => _SendScreenState();
+}
+
+class _SendScreenState extends State<SendScreen> {
+  var _selectedContact = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +25,86 @@ class SendScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Send'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Tell,",
+                  style: TextStyle(fontSize: 28),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                MenuAnchor(
+                  builder: (context, controller, child) {
+                    return ElevatedButton(
+                      onPressed: () {
+                        if (controller.isOpen) {
+                          controller.close();
+                        } else {
+                          controller.open();
+                        }
+                      },
+                      child: const Text(
+                        "Mom",
+                        style: TextStyle(
+                          fontSize: 28,
+                        ),
+                      ),
+                    );
+                  },
+                  menuChildren: const [
+                    MenuItemButton(
+                      child: Text("Mom"),
+                    ),
+                    MenuItemButton(
+                      child: Text("Mom"),
+                    ),
+                    MenuItemButton(
+                      child: Text("Mom"),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Center(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(5),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100000),
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "FUCK YOU",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
